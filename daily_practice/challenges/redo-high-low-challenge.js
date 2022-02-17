8a39f357de37e9c0be6f680a7cbd13aa743b0c8b
Matthew Ramirez <mattramirez244@gmail.com>
2022-02-17 14:15:59 -0800

    finished redo line by line, tried changing SPACE_BAR to ENTER_KEY, still uses SPACE_BAR

--- daily_practice/challenges/redo-high-low-challenge.js
+++ daily_practice/challenges/redo-high-low-challenge.js
@@ -20,41 +20,68 @@ function initializeGame(){
   numOfAttempts = 0
 }
 
+// function that takes in a UserNum as an argument
+function highLowInputResponse(userNumber) {
+  // if numbers are greater than 100 or less than 100 have them try again with correct numbers 
+  if(userNumber > 100 || userNumber < 1){
+    return `${userNumber} is greater than 100 or less than 1, please try again with appropirate numbers`
+  }
+  // if userNumber is equal to randomNumber generated -1, +1, or <, >, appropriate response to each. else if userName === RandomNumber generated it is correct
+  if(userNumber === randomNumber -1){
+    return `${userNumber} is low, but so close, keep trying!`
+  } else if(userNumber === randomNumber +1){
+    return `${userNumber} is high, but so close, keep trying!`
+  } else if(userNumber < randomNumber){
+    return `${userNumber} is lower, but keep trying!`
+  } else if(userNumber > randomNumber){
+    return `${userNumber} is higher, but keep trying!`
+  } else if(userNumber === randomNumber){
+    return `${userNumber} is correct, great Job!`
+  }
+}
 
 // function to set the amount of guesses to be 10 with an alert, connecting to html file, and adding in the guess into the form
 function guessAttempts() {
-  // if the number of attempts is incremented more than 10, send alert saying what the right number was and also re-initializeGame
+  // if the number of attempts is incremented more than 10, send alert saying what the right number was and then re-initializeGame
   if(++numOfAttempts > 10){
     alert(`Too many guesses! Correct number was ${randomNumber}, try again!`)
-    // re-initializeGame number
     initializeGame();
     return
   }
 
-  // local variable selecting first element in document.querySelector #guess connects to high-low-challenge.html inputed value
- 
-  // if the typeof guess === 'string', guess = the parseInt(guess)
- 
-  // initialize result to highLowInput function below with inputed guess
-  // initilize output to connect to element in high-low-challenge.html #output
-  // sending outputs content as the result from HighLowInput(guess)
+  // ***Connceting my redo-high-low-challenge.js functionality to my redo-high-low-challenge.html
 
+  // local variable selecting first element in document.querySelector #guess connects to high-low-challenge.html inputed value
+ let guess = document.querySelector('#guess').value
+  // if the typeof guess === 'string', guess = the parseInt(guess)
+  if(typeof guess === 'string'){
+    guess = ParseInt(guess)
+  }
+  // initialize result to highLowInput function below with inputed guess
+  const result = highLowInputResponse(guess)
+  // initilize output to connect to element in high-low-challenge.html #output
+  const output = documnt.querySelector('#output')
+    // sending outputs content as the result from HighLowInput(guess)
+    output.textContent = result
 }
- 
+
+// ***Connceting my redo-high-low-challenge.js functionality to my redo-high-low-challenge.html
 
 // connecting to html #guess to listen to event "keyup" event
-  // if the event keycode on the spacebar, which is key 32, initiate gradeTheGuess funtion for everytime SPACE_KEY is pressed
-
+document.querySelector('#guessBtn').input.addEventListener('keyup', function(event){
+  // if the event keycode on the enter key, which is key 13, initiate gradeTheGuess funtion for everytime ENTER_KEY is pressed
+  if(event.keyCode === ENTER_KEY) {
+    event.preventDefault();
+    guessAttempts()
+  }
+})
 
 // connect to html #guessbtn to listen to event 'click'
+document.querySelector('#guessBtn').input.addEventListener('click', function(event){
   // prevent default action to now intiatiate gradeTheGuess function
-
-
-// function that takes in a UserNum as an argument
-
-  // if numbers are greater than 100 or less than 100 have them try again with correct numbers 
-
-  // if userNumber is equal to randomNumber generated -1, +1, or <, >, appropriate response to each. else if userName === RandomNumber generated it is correct
-
+event.preventDefault()
+guessAttempts()
+})
 
 // re-initializeGame function everytime we run through the game
+initializeGame()
